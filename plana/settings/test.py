3c5f2cdf22a5e196d1d1dde02a6fd181fba6aea7
Carlo Spiga <cspiga@unistra.fr>
2022-10-19 15:18:52 +0200

    :tada: Initial commit - django-drybones cloned and initialized.

---
+++ plana/settings/test.py
@@ -0,0 +1,46 @@
+from .base import *
+
+
+#######################
+# Debug configuration #
+#######################
+
+DEBUG = True
+
+
+##########################
+# Database configuration #
+##########################
+
+DATABASES['default']['HOST'] = '{{ default_db_host }}'
+DATABASES['default']['USER'] = '{{ default_db_user }}'
+DATABASES['default']['PASSWORD'] = '{{ default_db_password }}'
+DATABASES['default']['NAME'] = '{{ default_db_name }}'
+
+
+############################
+# Allowed hosts & Security #
+############################
+
+ALLOWED_HOSTS = [
+    '.u-strasbg.fr',
+    '.unistra.fr',
+]
+
+
+#####################
+# Log configuration #
+#####################
+
+LOGGING['handlers']['file']['filename'] = '{{ remote_current_path }}/log/app.log'
+
+for logger in LOGGING['loggers']:
+    LOGGING['loggers'][logger]['level'] = 'DEBUG'
+
+
+############
+# Dipstrap #
+############
+
+DIPSTRAP_VERSION = '{{ dipstrap_version }}'
+DIPSTRAP_STATIC_URL += '%s/' % DIPSTRAP_VERSION
---
+++ plana/settings/unittest.py
@@ -0,0 +1,43 @@
+from os import environ
+from os.path import normpath
+from .base import *
+
+#######################
+# Debug configuration #
+#######################
+
+DEBUG = True
+
+
+##########################
+# Database configuration #
+##########################
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': environ.get('DEFAULT_DB_TEST_NAME', 'plana'),
+        'USER': environ.get('DEFAULT_DB_TEST_USER', 'plana'),
+        'PASSWORD': environ.get('DEFAULT_DB_TEST_PASSWORD', 'plana'),
+        'HOST': environ.get('DEFAULT_DB_TEST_HOST', 'postgres'),
+        'PORT': environ.get('DEFAULT_DB_TEST_PORT', ''),
+    }
+}
+
+############################
+# Allowed hosts & Security #
+############################
+
+ALLOWED_HOSTS = ['*']
+
+#####################
+# Log configuration #
+#####################
+
+LOGGING['handlers']['file']['filename'] = environ.get('LOG_DIR', normpath(join('/tmp', 'test_%s.log' % SITE_NAME)))
+LOGGING['handlers']['file']['level'] = 'DEBUG'
+
+for logger in LOGGING['loggers']:
+    LOGGING['loggers'][logger]['level'] = 'DEBUG'
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
