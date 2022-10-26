cf3befa3f73e62972980ba37401a6490c341f793
Carlo Spiga <cspiga@unistra.fr>
2022-10-26 10:44:23 +0200

    🗃️ Created User and Association models.

--- plana/apps/associations/models.py
+++ plana/apps/associations/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+class Association(models.Model):
+    """
+    Main table which represents an association.
+    """
+    username_association = models.CharField(default="", max_length=250, blank=False, unique=True)
+    name_association = models.CharField(default="", max_length=250, blank=False)
+    acronym_association = models.CharField(default="", max_length=30)
+    path_logo_association = models.CharField(default="", max_length=250)
+    description_association = models.TextField(default="")
+    activities_association = models.TextField(default="")
+    address_association = models.TextField(default="")
+    phone_association = models.CharField(default="", max_length=25)
+    email_association = models.CharField(default="", max_length=256)
+    siret_association = models.IntegerField(default=0)
+    website_association = models.CharField(default="", max_length=200)
+    student_amount_association = models.IntegerField(default=0)
+    is_enabled_association = models.BooleanField(default=False)
+    is_site_association = models.BooleanField(default=False)
+    created_date_association = models.DateTimeField(auto_now_add=True)
+    approval_date_association = models.DateTimeField(null=True) # date d'agrément
+    last_goa_date_association = models.DateTimeField(null=True) # date de dernière AGO
+    cga_date_association = models.DateTimeField(null=True) # date d'AG constitutive
+    institution = models.ForeignKey('Institution', on_delete=models.RESTRICT)
+    institution_component = models.ForeignKey('InstitutionComponent', on_delete=models.RESTRICT)
+    activity_field = models.ForeignKey('ActivityField', on_delete=models.RESTRICT)
+
+class SocialNetwork(models.Model):
+    """
+    Social networks from associations.
+    """
+    type_social_network = models.CharField(max_length=32, blank=False)
+    location_social_network = models.CharField(max_length=200, blank=False)
+    association = models.ForeignKey('Association', on_delete=models.CASCADE)
+
+class Institution(models.Model):
+    """
+    Associations are attached to institutions.
+    """
+    name_institution = models.CharField(max_length=250, blank=False)
+    acronym_institution = models.CharField(max_length=30, blank=False)
+
+class InstitutionComponent(models.Model):
+    """
+    Associations are attached to components.
+    """
+    name_institution_component = models.CharField(max_length=250, blank=False)
+
+class ActivityField(models.Model):
+    """
+    Associations have an activity field.
+    """
+    name_activity_field = models.CharField(max_length=250, blank=False)
+
--- plana/apps/users/models.py
+++ plana/apps/users/models.py
@@ -5,5 +5,17 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """
     Extending the abstract User class.
+    Following fields from Django User class are used :
+        - username
+        - password
+        - email
+        - first_name
+        - last_name
+        - is_active
     """
-    pass
+    is_cas_user = models.BooleanField(default=False)
+    # TODO token_reset_date_user = models.DateField(default=None)
+
+    class Meta:
+        default_permissions = []
+
