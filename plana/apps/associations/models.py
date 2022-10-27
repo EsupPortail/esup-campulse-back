091fc1288229499e9022ffc1c47f64837f7c08e6
Carlo Spiga <cspiga@unistra.fr>
2022-10-27 14:10:41 +0200

    🗃️ Database fields renamed to be shorter.

--- plana/apps/associations/models.py
+++ plana/apps/associations/models.py
@@ -4,24 +4,24 @@ class Association(models.Model):
     """
     Main table which represents an association.
     """
-    username_association = models.CharField(default="", max_length=250, blank=False, unique=True)
-    name_association = models.CharField(default="", max_length=250, blank=False)
-    acronym_association = models.CharField(default="", max_length=30)
-    path_logo_association = models.CharField(default="", max_length=250)
-    description_association = models.TextField(default="")
-    activities_association = models.TextField(default="")
-    address_association = models.TextField(default="")
-    phone_association = models.CharField(default="", max_length=25)
-    email_association = models.CharField(default="", max_length=256)
-    siret_association = models.IntegerField(default=0)
-    website_association = models.CharField(default="", max_length=200)
-    student_amount_association = models.IntegerField(default=0)
-    is_enabled_association = models.BooleanField(default=False)
-    is_site_association = models.BooleanField(default=False)
-    created_date_association = models.DateTimeField(auto_now_add=True)
-    approval_date_association = models.DateTimeField(null=True) # date d'agrément
-    last_goa_date_association = models.DateTimeField(null=True) # date de dernière AGO
-    cga_date_association = models.DateTimeField(null=True) # date d'AG constitutive
+    username = models.CharField(default="", max_length=250, blank=False, unique=True)
+    name = models.CharField(default="", max_length=250, blank=False)
+    acronym = models.CharField(default="", max_length=30)
+    path_logo = models.CharField(default="", max_length=250)
+    description = models.TextField(default="")
+    activities = models.TextField(default="")
+    address = models.TextField(default="")
+    phone = models.CharField(default="", max_length=25)
+    email = models.CharField(default="", max_length=256)
+    siret = models.IntegerField(default=0)
+    website = models.CharField(default="", max_length=200)
+    student_amount = models.IntegerField(default=0)
+    is_enabled = models.BooleanField(default=False)
+    is_site = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    approval_date = models.DateTimeField(null=True) # date d'agrément
+    last_goa_date = models.DateTimeField(null=True) # date de dernière AGO
+    cga_date = models.DateTimeField(null=True) # date d'AG constitutive
     institution = models.ForeignKey('Institution', on_delete=models.RESTRICT)
     institution_component = models.ForeignKey('InstitutionComponent', on_delete=models.RESTRICT)
     activity_field = models.ForeignKey('ActivityField', on_delete=models.RESTRICT)
@@ -30,26 +30,26 @@ class SocialNetwork(models.Model):
     """
     Social networks from associations.
     """
-    type_social_network = models.CharField(max_length=32, blank=False)
-    location_social_network = models.CharField(max_length=200, blank=False)
+    type = models.CharField(max_length=32, blank=False)
+    location = models.CharField(max_length=200, blank=False)
     association = models.ForeignKey('Association', on_delete=models.CASCADE)
 
 class Institution(models.Model):
     """
     Associations are attached to institutions.
     """
-    name_institution = models.CharField(max_length=250, blank=False)
-    acronym_institution = models.CharField(max_length=30, blank=False)
+    name = models.CharField(max_length=250, blank=False)
+    acronym = models.CharField(max_length=30, blank=False)
 
 class InstitutionComponent(models.Model):
     """
     Associations are attached to components.
     """
-    name_institution_component = models.CharField(max_length=250, blank=False)
+    name = models.CharField(max_length=250, blank=False)
 
 class ActivityField(models.Model):
     """
     Associations have an activity field.
     """
-    name_activity_field = models.CharField(max_length=250, blank=False)
+    name = models.CharField(max_length=250, blank=False)
 
