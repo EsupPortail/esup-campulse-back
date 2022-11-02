3ee8c071cda54c0e093e756ba29d8f818c137471
dbessey <daniel.bessey@unistra.fr>
2022-11-02 14:52:36 +0100

    Remove redundant urls

--- plana/urls.py
+++ plana/urls.py
@@ -15,9 +15,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-
     path('admin/', admin.site.urls),
 ]
 
