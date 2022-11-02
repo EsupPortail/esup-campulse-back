2b495172a42fee4603421b712b155192a25ceec4
lea.stroh <lea.stroh@unistra.fr>
2022-11-02 10:04:14 +0100

    🐛 Fixed missing DRF import in associations views

--- plana/apps/users/views.py
+++ plana/apps/users/views.py
@@ -1,7 +1,9 @@
 from allauth_cas.views import CASCallbackView, CASLoginView, CASLogoutView
 from django.shortcuts import get_object_or_404
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics, status
 
 from .adapter import CASAdapter
 from .models import User
