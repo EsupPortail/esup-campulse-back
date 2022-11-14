40510ff8efe8033d608b299910af1da5b84eca38
Carlo Spiga <cspiga@unistra.fr>
2022-11-14 16:17:53 +0100

    ✨ Added reset password mail template.

--- plana/templatetags/plana_tags.py
+++ plana/templatetags/plana_tags.py
@@ -1,3 +1,5 @@
+import re
+
 from django import template
 from django.conf import settings
 
@@ -6,7 +8,26 @@ register = template.Library()
 
 @register.simple_tag
 def settings_get(name):
+    """
+    Get a Django setting key.
+    """
     try:
         return str(settings.__getattr__(name))
     except Exception:
         return ""
+
+
+@register.filter(name="uid")
+def uid(url):
+    """
+    Tag used in password_reset_key_message email, represents user id in hex format.
+    """
+    return re.match(r"^(.*)/(.*)/(.*)/$", url).group(2)
+
+
+@register.filter(name="token")
+def token(url):
+    """
+    Tag used in password_reset_key_message email, represents token to reset a password.
+    """
+    return re.match(r"^(.*)/(.*)/(.*)/$", url).group(3)
