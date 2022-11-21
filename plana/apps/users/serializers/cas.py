4b70deec1f917b6349d384dd40b65bcb8df346b6
Carlo Spiga <cspiga@unistra.fr>
2022-11-21 15:33:42 +0100

    🚧 Removed CAS registration validation on login.

--- plana/apps/users/serializers/cas.py
+++ plana/apps/users/serializers/cas.py
@@ -72,6 +72,8 @@ class CASSerializer(LoginSerializer):
                 )
         else:
             attrs["user"] = login.account.user
+            # TODO Avoid logging in a CAS user who hasn't completed registration.
+            """
             user = User.objects.get(email=attrs["user"].email)
             try:
                 user.groups.all()[0]
@@ -79,6 +81,7 @@ class CASSerializer(LoginSerializer):
                 raise serializers.ValidationError(
                     _("Account registration must be completed.")
                 )
+            """
 
         return attrs
 
