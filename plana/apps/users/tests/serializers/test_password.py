44c34c7f0745111073e9b7fd839d4ba7546cec64
Carlo Spiga <cspiga@unistra.fr>
2022-11-25 11:06:32 +0100

    ✅ Added Password Reset Serializer tests.

--- plana/apps/users/tests/serializers/test_password.py
+++ plana/apps/users/tests/serializers/test_password.py
@@ -5,7 +5,10 @@ from rest_framework.test import APIRequestFactory, force_authenticate
 from allauth.socialaccount.models import SocialAccount
 
 from plana.apps.users.models.user import User
-from plana.apps.users.serializers.user import PasswordChangeSerializer
+from plana.apps.users.serializers.user import (
+    PasswordChangeSerializer,
+    PasswordResetSerializer,
+)
 
 
 class TestLocalUserPasswordChangeSerializer(TestCase):
@@ -20,15 +23,11 @@ class TestLocalUserPasswordChangeSerializer(TestCase):
             email="georges@saucisse.fr",
             is_validated_by_admin=True,
         )
-        passwords = {
-            "new_password1": "passedemot",
-            "new_password2": "passedemot",
-        }
-        request = APIRequestFactory().post(passwords, format="json")
+
+        request = APIRequestFactory().post({}, format="json")
         force_authenticate(request, user)
         request.user = user
 
-        cls.request_data = passwords
         cls.request = request
         cls.user = user
 
@@ -36,11 +35,43 @@ class TestLocalUserPasswordChangeSerializer(TestCase):
         """
         Raises an AttributeError because form is valid.
         """
+        passwords = {"new_password1": "passedemot", "new_password2": "passedemot"}
         serializer = PasswordChangeSerializer(
             reverse("rest_password_change"),
-            context={"request": self.request},
+            context={"request": self.request, "data": passwords},
         )
-        serializer.validate(self.request_data)
+        serializer.validate(passwords)
+        with self.assertRaises(AttributeError):
+            serializer.save()
+
+
+class TestLocalUserPasswordResetSerializer(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(
+            username="Georges",
+            email="georges@saucisse.fr",
+            is_validated_by_admin=True,
+        )
+
+        request = APIRequestFactory().post({}, format="json")
+        request.data = {"email": "georges@saucisse.fr"}
+
+        cls.request = request
+        cls.user = user
+
+    def test_reset_password_as_local_user(self):
+        passwords = {
+            "new_password1": "passedemot",
+            "new_password2": "passedemot",
+            "uid": hex(self.user.id),
+            "token": "0123456789abcdef",
+        }
+        serializer = PasswordResetSerializer(
+            reverse("rest_password_reset_confirm"),
+            context={"request": self.request, "data": passwords},
+        )
+        serializer.validate(passwords)
         with self.assertRaises(AttributeError):
             serializer.save()
 
@@ -60,15 +91,11 @@ class TestCASUserPasswordChangeSerializer(TestCase):
         social_account = SocialAccount.objects.create(
             provider="cas", uid="GeorgesCAS", user_id=user.id
         )
-        passwords = {
-            "new_password1": "passedemot",
-            "new_password2": "passedemot",
-        }
-        request = APIRequestFactory().post(passwords, format="json")
+
+        request = APIRequestFactory().post({}, format="json")
         force_authenticate(request, user)
         request.user = user
 
-        cls.request_data = passwords
         cls.request = request
         cls.user = user
 
@@ -76,9 +103,46 @@ class TestCASUserPasswordChangeSerializer(TestCase):
         """
         Raises a ValidationError triggered by the custom Serializer.
         """
+        passwords = {
+            "new_password1": "passedemot",
+            "new_password2": "passedemot",
+        }
         serializer = PasswordChangeSerializer(
             reverse("rest_password_change"),
-            context={"request": self.request},
+            context={"request": self.request, "data": passwords},
+        )
+        with self.assertRaises(ValidationError):
+            serializer.save()
+
+
+class TestCASUserPasswordResetSerializer(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(
+            username="GeorgesCAS",
+            email="georges-cas@unistra.fr",
+            is_validated_by_admin=True,
+        )
+        social_account = SocialAccount.objects.create(
+            provider="cas", uid="GeorgesCAS", user_id=user.id
+        )
+
+        request = APIRequestFactory().post({}, format="json")
+        request.data = {"email": "georges-cas@unistra.fr"}
+
+        cls.request = request
+        cls.user = user
+
+    def test_reset_password_as_cas_user(self):
+        passwords = {
+            "new_password1": "passedemot",
+            "new_password2": "passedemot",
+            "uid": hex(self.user.id),
+            "token": "0123456789abcdef",
+        }
+        serializer = PasswordResetSerializer(
+            reverse("rest_password_reset_confirm"),
+            context={"request": self.request, "data": passwords},
         )
         with self.assertRaises(ValidationError):
             serializer.save()
