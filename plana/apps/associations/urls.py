d97bc4d3f6d4d43ec57cf403fcc4ed7673e5b959
Carlo Spiga <cspiga@unistra.fr>
2022-11-29 14:13:29 +0100

    💩 Added useless import for SocialNetwork, otherwise migrations broke.

--- plana/apps/associations/urls.py
+++ plana/apps/associations/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 
-# from rest_framework import routers
 from .views.association import AssociationList, AssociationDetail
 
-# router = routers.DefaultRouter()
-# router.register(r'associations', views.AssociationView)
 
 urlpatterns = [
     path("", AssociationList.as_view(), name="association_list"),
