90790dfb4153d159f121504e50ec0f942d8b21a1
Carlo Spiga <cspiga@unistra.fr>
2023-02-10 11:14:07 +0100

    ✨ Added permissions to /groups .

--- plana/apps/groups/serializers/group.py
+++ plana/apps/groups/serializers/group.py
@@ -2,6 +2,8 @@
 Serializers describing fields used on auth groups.
 """
 from django.contrib.auth.models import Group
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 
@@ -10,9 +12,17 @@ class GroupSerializer(serializers.ModelSerializer):
     Main serializer.
     """
 
+    permissions = serializers.SerializerMethodField()
+
+    @extend_schema_field(OpenApiTypes.OBJECT)
+    def get_permissions(self, group):
+        """
+        Return permissions links.
+        """
+        return Group.objects.get(id=group.id).permissions.values_list(
+            'codename', flat=True
+        )
+
     class Meta:
         model = Group
-        fields = (
-            "id",
-            "name",
-        )
+        fields = ("id", "name", "permissions")
