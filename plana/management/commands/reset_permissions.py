e72959274107e5c30d8c07f1fc343f28f9f605d5
Carlo Spiga <cspiga@unistra.fr>
2023-02-13 11:48:30 +0100

    🧑‍💻 Updated reset_permissions command to regenerate fixtures automatically.

--- plana/management/commands/reset_permissions.py
+++ plana/management/commands/reset_permissions.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import Group, Permission
+from django.core import management
 from django.core.management.base import BaseCommand
 
 from plana.settings.permissions import PERMISSIONS_GROUPS
@@ -16,4 +17,16 @@ class Command(BaseCommand):
                 group.permissions.add(
                     Permission.objects.get(codename=new_group_permission)
                 )
+        management.call_command(
+            'dumpdata',
+            'auth.permission',
+            indent=2,
+            output='plana/apps/groups/fixtures/auth_permission.json',
+        )
+        management.call_command(
+            'dumpdata',
+            'auth.group_permissions',
+            indent=2,
+            output='plana/apps/groups/fixtures/auth_group_permissions.json',
+        )
         self.stdout.write("Updated all group permissions.")
