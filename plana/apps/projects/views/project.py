6232671e2584ee1b424dda27ff75c576b5ac608c
lea.stroh <lea.stroh@unistra.fr>
2023-03-15 08:40:22 +0100

    🚧 Moved project categories views in a different file

--- plana/apps/projects/views/project.py
+++ plana/apps/projects/views/project.py
@@ -1,15 +1,15 @@
-"""Views directly linked to projects."""
-from rest_framework import generics
-
-from plana.apps.projects.models.category import Category
-from plana.apps.projects.serializers.category import CategorySerializer
-
-
-class CategoryList(generics.ListAPIView):
-    """Lists all Categories."""
-
-    serializer_class = CategorySerializer
-
-    def get_queryset(self):
-        """GET : Lists all categories."""
-        return Category.objects.all().order_by("name")
+# """Views directly linked to projects."""
+# from rest_framework import generics
+#
+# from plana.apps.projects.models.category import Project
+# from plana.apps.projects.serializers.category import ProjectSerializer
+#
+#
+# class ProjectList(generics.ListAPIView):
+#    """Lists all Categories."""
+#
+#    serializer_class = ProjectSerializer
+#
+#    def get_queryset(self):
+#        """GET : Lists all projects."""
+#        return Project.objects.all().order_by("name")
