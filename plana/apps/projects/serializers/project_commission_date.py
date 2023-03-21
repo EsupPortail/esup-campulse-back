704d13c1a397d1f25a0167a4580463a82c59c89e
lea.stroh <lea.stroh@unistra.fr>
2023-03-21 10:56:49 +0100

    ♻️ Updated ProjectCommissionDate serializer

--- plana/apps/projects/serializers/project_commission_date.py
+++ plana/apps/projects/serializers/project_commission_date.py
@@ -1,6 +1,7 @@
 """Serializers describing fields used on project commission date table."""
 from rest_framework import serializers
 
+from plana.apps.commissions.models.commission_date import CommissionDate
 from plana.apps.commissions.serializers.commission_date import CommissionDateSerializer
 from plana.apps.projects.models.project import Project
 from plana.apps.projects.models.project_commission_date import ProjectCommissionDate
@@ -12,7 +13,9 @@ class ProjectCommissionDateSerializer(serializers.ModelSerializer):
     project = serializers.SlugRelatedField(
         slug_field="id", queryset=Project.objects.all()
     )
-    commission_date = CommissionDateSerializer()
+    commission_date = serializers.SlugRelatedField(
+        slug_field="id", queryset=CommissionDate.objects.all()
+    )
 
     class Meta:
         model = ProjectCommissionDate
