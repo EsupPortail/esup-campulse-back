a6e0cfba2e5fa00ea6e497d695336eebe908f2e0
Carlo Spiga <cspiga@unistra.fr>
2023-04-18 08:12:35 +0200

    🔖 Tag 0.1.20230418

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ recursive_requirements('requirements.txt', libraries, dependency_links)
 
 setup(
     name='plana',
-    version='0.1.20230417',
+    version='0.1.20230418',
     packages=find_packages(),
     install_requires=libraries,
     dependency_links=dependency_links,
