aab63d1aabd22f243d27b893fd1205b9ca87d05f
Carlo Spiga <cspiga@unistra.fr>
2023-04-24 16:18:22 +0200

    🔧 Added age package to fabfile init.

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -40,9 +40,11 @@ env.verbose_output = False  # True for verbose output
 # env.dest_path = '' # if not set using env_local_tmp_dir
 # env.excluded_files = ['pron.jpg'] # file(s) that rsync should exclude when deploying app
 # env.extra_ppa_to_install = ['ppa:vincent-c/ponysay'] # extra ppa source(s) to use
-# env.extra_pkg_to_install = ['ponysay'] # extra debian/ubuntu package(s) to install on remote
+# extra debian/ubuntu package(s) to install on remote
+env.extra_pkg_to_install = ['age']
 # env.cfg_shared_files = ['config','/app/path/to/config/config_file'] # config files to be placed in shared config dir
-env.extra_symlink_dirs = ['keys']  # dirs to be symlinked in shared directory
+# dirs to be symlinked in shared directory
+env.extra_symlink_dirs = ['keys']
 # env.verbose = True # verbose display for pydiploy default value = True
 # env.req_pydiploy_version = "0.9" # required pydiploy version for this fabfile
 # env.no_config_test = False # avoid config checker if True
