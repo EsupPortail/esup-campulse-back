eb82c03810dde3cede7d5c2f826549fb73d167d7
Carolane Lefebvre <carolane.lefebvre@unistra.fr>
2023-05-03 12:01:35 +0200

    :construction: WIP: project comment view

--- plana/apps/projects/views/project_comment.py
+++ plana/apps/projects/views/project_comment.py
@@ -2,13 +2,39 @@
 from rest_framework import generics, response, status
 from rest_framework.permissions import DjangoModelPermissions, IsAuthenticated
 
+from drf_spectacular.utils import OpenApiParameter, extend_schema
+from drf_spectacular.types import OpenApiTypes
+
 from plana.apps.projects.models.project_comment import ProjectComment
 from plana.apps.projects.serializers.project_comment import (ProjectCommentSerializer, ProjectCommentDataSerializer)
 
 
-class ProjectCommentCreate(generics.ListCreateAPIView):
+class ProjectCommentListCreate(generics.ListCreateAPIView):
     """/projects/comments route"""
 
     permission_classes = [IsAuthenticated, DjangoModelPermissions]
     queryset = ProjectComment.objects.all()
     serializer_class = ProjectCommentSerializer
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "project_id",
+                OpenApiTypes.NUMBER,
+                OpenApiParameter.QUERY,
+                description="Project id."
+            ),
+        ],
+        responses={
+            status.HTTP_200_OK: ProjectCommentSerializer,
+            status.HTTP_401_UNAUTHORIZED: None,
+            status.HTTP_403_FORBIDDEN: None,
+        },
+        tags=["projects/comments"]
+    )
+    def get(self, request, *args, **kwargs):
+        """Lists all links between projects and comments"""
+        project_id = request.query_params.get("project_id")
+
+        if project_id:
+            self.queryset = self.queryset.filter(project_id=project_id)
