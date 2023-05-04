bfda40beb091c38a07d9707f54eab7c8a4b48fa8
Carolane Lefebvre <carolane.lefebvre@unistra.fr>
2023-05-04 09:52:53 +0200

    :sparkles: feat!: project comment list create (views/project_comment)

--- plana/apps/projects/views/project_comment.py
+++ plana/apps/projects/views/project_comment.py
@@ -1,4 +1,9 @@
 """Views directly linked to projects comments."""
+import datetime
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import gettext_lazy as _
+
 from rest_framework import generics, response, status
 from rest_framework.permissions import DjangoModelPermissions, IsAuthenticated
 
@@ -6,6 +11,7 @@ from drf_spectacular.utils import OpenApiParameter, extend_schema
 from drf_spectacular.types import OpenApiTypes
 
 from plana.apps.projects.models.project_comment import ProjectComment
+from plana.apps.projects.models.project import Project
 from plana.apps.projects.serializers.project_comment import (ProjectCommentSerializer, ProjectCommentDataSerializer)
 
 
@@ -41,7 +47,6 @@ class ProjectCommentListCreate(generics.ListCreateAPIView):
 
         return self.list(request, *args, **kwargs)
 
-
     @extend_schema(
         responses={
             status.HTTP_201_CREATED: ProjectCommentSerializer,
@@ -51,5 +56,28 @@ class ProjectCommentListCreate(generics.ListCreateAPIView):
         },
         tags=["project/comments"],
     )
-    def post(self, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         """Create a link between a comment and a project"""
+        try:
+            project = Project.objects.get(pk=request.data["project"])
+            request.data["creation_date"] = datetime.date.today()
+        except ObjectDoesNotExist:
+            return response.Response(
+                {"error": _("Project does not exist.")},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        if not request.user.has_perm("projects.add_projectcomment"):
+            return response.Response(
+                {"error": _("Not allowed to add comments for this project.")},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        try:
+            ProjectComment.objects.get(
+                project_id=request.data["project"], text=request.data["text"]
+            )
+        except ObjectDoesNotExist:
+            return super().create(request, *args, **kwargs)
+
+        return response.Response({}, status=status.HTTP_200_OK)
