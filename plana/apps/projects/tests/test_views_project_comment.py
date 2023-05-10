63afb87227c5ba272755214cef686f0c5b354362
Carolane Lefebvre <carolane.lefebvre@unistra.fr>
2023-05-10 08:46:08 +0200

    :test_tube: tests: added failing tests
    test_delete_project_categories_association_success, test_get_project_comments_by_id and test_get_project_comments_by_id_401

--- plana/apps/projects/tests/test_views_project_comment.py
+++ plana/apps/projects/tests/test_views_project_comment.py
@@ -5,8 +5,11 @@ from django.test import Client, TestCase
 from django.urls import reverse
 from rest_framework import status
 
+from plana.apps.institutions.models import Institution
+from plana.apps.associations.models.association import Association
 from plana.apps.projects.models.project_comment import ProjectComment
 from plana.apps.projects.models.project import Project
+from plana.apps.users.models import GroupInstitutionCommissionUser
 
 
 class ProjectCommentLinksViewsTests(TestCase):
@@ -33,7 +36,7 @@ class ProjectCommentLinksViewsTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        """Start clients used on tests"""
+        """Start clients used on tests."""
         cls.client = Client()
         url_login = reverse("rest_login")
 
@@ -42,9 +45,27 @@ class ProjectCommentLinksViewsTests(TestCase):
         cls.general_client = Client()
         data_general = {
             "username": cls.manager_general_user_name,
-            "password": "motdepasse"
+            "password": "motdepasse",
         }
         cls.response = cls.general_client.post(url_login, data_general)
+
+        cls.manager_institution_user_id = 4
+        cls.manager_institution_user_name = "gestionnaire-uha@mail.tld"
+        cls.institution_client = Client()
+        data_institution = {
+            "username": cls.manager_institution_user_name,
+            "password": "motdepasse",
+        }
+        cls.response = cls.institution_client.post(url_login, data_institution)
+
+        cls.student_offsite_user_id = 10
+        cls.student_offsite_user_name = "etudiant-asso-hors-site@mail.tld"
+        cls.student_offsite_client = Client()
+        data_student_offsite = {
+            "username": cls.student_offsite_user_name,
+            "password": "motdepasse",
+        }
+        cls.response = cls.student_offsite_client.post(url_login, data_student_offsite)
 
         cls.student_president_user_id = 13
         cls.student_president_user_name = "president-asso-site@mail.tld"
@@ -57,11 +78,193 @@ class ProjectCommentLinksViewsTests(TestCase):
             url_login, data_student_president
         )
 
+    def test_get_project_comments_anonymous(self):
+        """
+        GET /projects/comments .
+
+        - An anonymous user cannot execute this request
+        """
+        response = self.client.get("/projects/comments")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
     def test_get_project_comments_student(self):
         """
-        GET /projects/categories .
+        GET /projects/comments
 
-        - A student user gets comments where projects rights are OK.
+        - A student user gets comments where projects rights are OK
         """
-        response = self.student_president_client.get("/projects/comments")
+        response = self.student_offsite_client.get("/projects/comments")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_project_comments_institution_manager(self):
+        """
+        GET /projects/comments
+
+        - An institution manager user gets project comments for correct projects
+        """
+        response = self.institution_client.get("/projects/comments")
+        user_institution_ids = Institution.objects.filter(
+            id__in=GroupInstitutionCommissionUser.objects.filter(
+                user_id=self.manager_institution_user_id
+            ).values_list("institution_id")
+        )
+        project_comments_cnt = ProjectComment.objects.filter(
+            project_id__in=Project.objects.filter(
+                association_id__in=Association.objects.filter(
+                    institution_id__in=user_institution_ids
+                ).values_list("id")
+            )
+        ).count()
+
+        content = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(content), project_comments_cnt)
+
+    def test_get_project_categories_manager(self):
+        """
+        GET /projects/comments .
+
+        - A general manager user gets all project categories.
+        - project_id argument filters by Project ID.
+        """
+        response = self.general_client.get("/projects/comments")
+        projects_categories_cnt = ProjectComment.objects.all().count()
+        content = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(content), projects_categories_cnt)
+
+        response = self.general_client.get("/projects/comments?project_id=2")
+        projects_categories_cnt = ProjectComment.objects.filter(project_id=2).count()
+        content = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(content), projects_categories_cnt)
+
+    def test_post_project_comment_not_found(self):
+        """
+        POST /projects/comments .
+
+        - The route can be accessed by a manager.
+        - The project must exist.
+        """
+        post_data = {
+            "project": 999,
+            "text": "Ce commentaire n'est pas censé exister.",
+        }
+        response = self.general_client.post("/projects/comments", post_data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_post_project_comment_forbidden_user(self):
+        """
+        POST /projects/comments
+
+        - The route cannot be accessed by a student user
+        """
+        post_data = {
+            "project": 1,
+            "text": "Le chiffre 6 c'est comme saucisse"
+        }
+        response = self.student_offsite_client.post("/projects/comments", post_data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_project_comments_by_id_anonymous(self):
+        """
+        GET /projects/{project_id}/comments
+
+        - An anonymous user cannot execute this request
+        """
+        response = self.client.get("/projects/1/comments")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_project_comments_by_id_404(self):
+        """
+        GET /projects/{project_id}/comments
+
+        - The route returns a 404 if a wrong project id is given
+        """
+        response = self.general_client.get("/projects/99999/comments")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_project_comments_by_id_401(self):
+        """
+        GET /projects/{project_id}/comments
+
+        - A student user not owning the project cannot execute this request
+        """
+        response = self.student_offsite_client.get("/projects/1/comments")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_project_comments_by_id(self):
+        """
+        GET /projects/{project_id}/comments
+
+        - The route can be accessed by a manager user and by a student user
+        - Correct projects comments are returned
+        """
+        project_id = 1
+        project_test_cnt = ProjectComment.objects.filter(project_id=project_id).count()
+        response = self.general_client.get(f"/projects/{project_id}/comments")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        content = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(len(content), project_test_cnt)
+
+        project_id = 2
+        project_test_cnt = ProjectComment.objects.filter(project_id=project_id).count()
+        response = self.student_president_client.get(f"/projects/{project_id}/comments")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        content = json.loads(response.content.decode("utf-8"))
+        self.assertEqual(len(content), project_test_cnt)
+
+    def test_delete_project_categories_anonymous(self):
+        """
+        DELETE /projects/{project_id}/comments/{comment_id} .
+
+        - An anonymous user cannot execute this request.
+        """
+        response = self.client.delete("/projects/1/comments/1")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_delete_project_categories_not_found(self):
+        """
+        DELETE /projects/{project_id}/comments/{comment_id} .
+
+        - The project must exist.
+        """
+        project = 999
+        comment = 1
+        response = self.general_client.delete(
+            f"/projects/{project}/comments/{comment}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_project_categories_forbidden_user(self):
+        """
+        DELETE /projects/{project_id}/categories/{category_id} .
+
+        - The route can be accessed by a student user.
+        - The owner of the project must be the authenticated user.
+         """
+        project = 1
+        comment = 5
+        response = self.student_offsite_client.delete(f"/projects/{project}/comments/{comment}")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_project_categories_association_success(self):
+        """
+        DELETE /projects/{project_id}/comments/{comment_id} .
+        """
+        project = 2
+        comment = 1
+        response = self.general_client.delete(
+            f"/projects/{project}/comments/{comment}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(
+            0,
+            len(ProjectComment.objects.filter(project=project, text=comment)),
+        )
+        response = self.general_client.delete(
+            f"/projects/{project}/comments/{comment}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
