d052070f6a48ca6e5c446eedd07afd485104432f
Carlo Spiga <cspiga@unistra.fr>
2023-05-25 11:45:26 +0200

    💩 Put problematic VisibleProjectManager code in a try catch.

--- plana/apps/projects/models/managers/visible_project_manager.py
+++ plana/apps/projects/models/managers/visible_project_manager.py
@@ -3,21 +3,24 @@ import datetime
 
 from django.conf import settings
 from django.db import models
+from django.db.utils import ProgrammingError
 
 
 class VisibleProjectManager(models.Manager):
     def get_queryset(self):
         queryset = super().get_queryset()
-        # TODO This breaks migrate command on new databases.
-        """
         invisible_projects_ids = []
-        for project in queryset:
-            if datetime.datetime.now(
-                datetime.timezone(datetime.timedelta(hours=0))
-            ) > project.edition_date + datetime.timedelta(
-                days=(365 * int(settings.AMOUNT_YEARS_BEFORE_PROJECT_INVISIBILITY))
-            ):
-                invisible_projects_ids.append(project.id)
-        queryset = queryset.exclude(id__in=invisible_projects_ids)
-        """
+        try:
+            for project in queryset:
+                if datetime.datetime.now(
+                    datetime.timezone(datetime.timedelta(hours=0))
+                ) > project.edition_date + datetime.timedelta(
+                    days=(365 * int(settings.AMOUNT_YEARS_BEFORE_PROJECT_INVISIBILITY))
+                ):
+                    invisible_projects_ids.append(project.id)
+            queryset = queryset.exclude(id__in=invisible_projects_ids)
+        except ProgrammingError:
+            # TODO Error triggered when initial migration is applied.
+            # Find a better way to manage this case.
+            pass
         return queryset
