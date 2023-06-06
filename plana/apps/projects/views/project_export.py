6e6d3f285ab96caaf204c57091d6625a171bb351
lea.stroh <lea.stroh@unistra.fr>
2023-06-06 15:22:33 +0200

    ♻️ Refactored project review summary pdf export

--- plana/apps/projects/views/project_export.py
+++ plana/apps/projects/views/project_export.py
@@ -124,84 +124,69 @@ class ProjectReviewDataExport(generics.RetrieveAPIView):
     )
     def get(self, request, *args, **kwargs):
         """Retrieves a PDF file."""
-        pass
+        try:
+            project = self.queryset.get(id=kwargs["id"])
+            data = project.__dict__
+        except ObjectDoesNotExist:
+            return response.Response(
+                {"error": _("Project does not exist.")},
+                status=status.HTTP_404_NOT_FOUND,
+            )
 
+        if (
+            not request.user.has_perm("projects.view_project_any_fund")
+            and not request.user.has_perm("projects.view_project_any_institution")
+            and not request.user.can_access_project(project)
+        ):
+            return response.Response(
+                {"error": _("Not allowed to retrieve this project.")},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
-#        try:
-#            project = self.queryset.get(id=kwargs["id"])
-#            data = project.__dict__
-#        except ObjectDoesNotExist:
-#            return response.Response(
-#                {"error": _("Project does not exist.")},
-#                status=status.HTTP_404_NOT_FOUND,
-#            )
-#
-#        if (
-#            not request.user.has_perm("projects.view_project_any_fund")
-#            and not request.user.has_perm("projects.view_project_any_institution")
-#            and not request.user.can_access_project(project)
-#        ):
-#            return response.Response(
-#                {"error": _("Not allowed to retrieve this project.")},
-#                status=status.HTTP_403_FORBIDDEN,
-#            )
-#
-#        if data["association_id"] is not None:
-#            data["association"] = Association.objects.get(
-#                id=data["association_id"]
-#            ).name
-#
-#        if data["user_id"] is not None:
-#            user = User.objects.get(id=data["user_id"])
-#            data["other_first_name"] = user.first_name
-#            data["other_last_name"] = user.last_name
-#            data["other_email"] = user.email
-#            data["other_phone"] = user.phone
-#
-#        data["project_commission_dates"] = list(
-#            ProjectCommissionFund.objects.filter(project_id=data["id"]).values(
-#                "commission_fund_id",
-#                "is_first_edition",
-#                "amount_asked_previous_edition",
-#                "amount_earned_previous_edition",
-#                "amount_asked",
-#                "amount_earned",
-#            )
-#        )
-#        commission_infos = list(
-#            Commission.objects.filter(
-#                pk__in=[
-#                    pcd["commission_fund_id"]
-#                    for pcd in data["project_commission_dates"]
-#                ]
-#            )
-#            .annotate(
-#                commission_acronym=Subquery(
-#                    Fund.objects.filter(id=OuterRef("commission_id")).values("acronym"),
-#                    output_field=models.CharField(),
-#                )
-#            )
-#            .values("commission_acronym", "commissionfund", "id")
-#        )
-#        for commission in commission_infos:
-#            for link in data["project_commission_dates"]:
-#                if commission["id"] == link["commission_fund_id"]:
-#                    link["commission_acronym"] = commission["commission_acronym"]
-#                    link["commission_fund"] = commission["commission_fund"]
-#
-#        data["is_first_edition"] = True
-#        for edition in data["project_commission_dates"]:
-#            if not edition["is_first_edition"]:
-#                data["is_first_edition"] = False
-#                break
-#
-#        data["documents"] = list(
-#            DocumentUpload.objects.filter(project_id=data["id"]).values(
-#                "name", "document__name"
-#            )
-#        )
-#
-#        # print(data)
-#        return generate_pdf(
-#            data, "project_review_summary", request.build_absolute_uri('/')
-#        )
+        if data["association_id"] is not None:
+            data["association"] = Association.objects.get(
+                id=data["association_id"]
+            ).name
+
+        if data["user_id"] is not None:
+            user = User.objects.get(id=data["user_id"])
+            data["other_first_name"] = user.first_name
+            data["other_last_name"] = user.last_name
+            data["other_email"] = user.email
+            data["other_phone"] = user.phone
+
+        data["project_commission_funds"] = list(
+            ProjectCommissionFund.objects.filter(project_id=data["id"]).values(
+                "commission_fund_id",
+                "is_first_edition",
+                "amount_asked_previous_edition",
+                "amount_earned_previous_edition",
+                "amount_asked",
+                "amount_earned",
+            )
+        )
+        for pcf in data["project_commission_funds"]:
+            pcf["commission_data"] = Commission.objects.get(
+                id=CommissionFund.objects.get(
+                    id=pcf["commission_fund_id"]
+                ).commission_id
+            ).__dict__
+            pcf["fund_data"] = Fund.objects.get(
+                id=CommissionFund.objects.get(id=pcf["commission_fund_id"]).fund_id
+            ).__dict__
+
+        data["is_first_edition"] = True
+        for edition in data["project_commission_funds"]:
+            if not edition["is_first_edition"]:
+                data["is_first_edition"] = False
+                break
+
+        data["documents"] = list(
+            DocumentUpload.objects.filter(project_id=data["id"]).values(
+                "name", "document__name"
+            )
+        )
+
+        return generate_pdf(
+            data, "project_review_summary", request.build_absolute_uri('/')
+        )
