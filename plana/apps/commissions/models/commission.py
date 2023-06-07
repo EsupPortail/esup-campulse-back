8cb955352d4d8a0efef018156fe4951b059fb05e
lea.stroh <lea.stroh@unistra.fr>
2023-06-07 16:06:43 +0200

    🗃️ Added name field to commission model

--- plana/apps/commissions/models/commission.py
+++ plana/apps/commissions/models/commission.py
@@ -9,9 +9,12 @@ class Commission(models.Model):
     submission_date = models.DateField(_("Project submission limit date"))
     commission_date = models.DateField(_("Commission date"))
     is_open_to_projects = models.BooleanField(_("Is open to projects"), default=False)
+    name = models.CharField(
+        _("Name"), max_length=250, blank=False, null=False, unique=True, default=""
+    )
 
     def __str__(self):
-        return f"{self.submission_date}, {self.commission_date}, {self.is_open_to_projects}"
+        return f"{self.name} : {self.submission_date}, {self.commission_date}, {self.is_open_to_projects}"
 
     class Meta:
         verbose_name = _("Commission")
