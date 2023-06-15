cef9cfddd6d28f23a253ec57b07be316d881767e
lea.stroh <lea.stroh@unistra.fr>
2023-06-15 10:33:16 +0200

    ✨ Added CSV generation with associations data

--- plana/apps/associations/views/association_export.py
+++ plana/apps/associations/views/association_export.py
@@ -1,3 +1,6 @@
+import csv
+
+from django.http import HttpResponse
 from django.utils.translation import gettext_lazy as _
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import OpenApiParameter, extend_schema
@@ -8,6 +11,7 @@ from plana.apps.associations.models.association import Association
 from plana.apps.associations.serializers.association import (
     AssociationAllDataReadSerializer,
 )
+from plana.apps.institutions.models import Institution, InstitutionComponent
 
 
 class AssociationsCSVExport(generics.RetrieveAPIView):
@@ -16,5 +20,39 @@ class AssociationsCSVExport(generics.RetrieveAPIView):
     serializer_class = AssociationAllDataReadSerializer
 
     def get(self, request, *args, **kwargs):
-        print("future CSV export view here")
-        return response.Response(status=status.HTTP_200_OK)
+        http_response = HttpResponse(
+            content_type="text/csv",
+            headers={"Content-Disposition": 'attachment; filename="associations.csv"'},
+        )
+        writer = csv.writer(http_response)
+        writer.writerow(
+            [
+                _("Name"),
+                _("Acronym"),
+                _("Institution"),
+                _("Activity Field"),
+                _("Institution Component"),
+                _("Last GOA Date"),
+                _("Email"),
+            ]
+        )
+        for association in self.get_queryset():
+            institution_component = (
+                None
+                if association.institution_component_id is None
+                else InstitutionComponent.objects.get(
+                    id=association.institution_component_id
+                ).name
+            )
+            writer.writerow(
+                [
+                    association.name,
+                    association.acronym,
+                    Institution.objects.get(id=association.institution_id).name,
+                    association.activity_field,
+                    institution_component,
+                    association.last_goa_date,
+                    association.email,
+                ]
+            )
+        return http_response
