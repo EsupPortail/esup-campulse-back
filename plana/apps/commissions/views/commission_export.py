1b0daf6d58075f8b2d7037986ff4f4bdb44b45db
lea.stroh <lea.stroh@unistra.fr>
2023-06-16 11:41:49 +0200

    🚧 Added first fields in commission projects csv export

--- plana/apps/commissions/views/commission_export.py
+++ plana/apps/commissions/views/commission_export.py
@@ -6,8 +6,16 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
-from plana.apps.projects.models import Project
+from plana.apps.associations.models import Association
+from plana.apps.commissions.models import Commission, CommissionFund, Fund
+from plana.apps.projects.models import (
+    Category,
+    Project,
+    ProjectCategory,
+    ProjectCommissionFund,
+)
 from plana.apps.projects.serializers.project import ProjectSerializer
+from plana.apps.users.models import User
 
 
 class CommissionProjectsCSVExport(generics.RetrieveAPIView):
@@ -17,12 +25,14 @@ class CommissionProjectsCSVExport(generics.RetrieveAPIView):
 
     def get(self, request, *args, **kwargs):
         """Projects presented to the commission CSV export."""
-        commission = kwargs["pk"]
+        queryset = self.get_queryset()
+        commission_id = kwargs["pk"]
 
+        funds = Fund.objects.all()
         http_response = HttpResponse(content_type="application/csv")
         http_response[
             "Content-Disposition"
-        ] = f"Content-Disposition: attachment; filename='commission_{commission}_export.csv'"
+        ] = f"Content-Disposition: attachment; filename='commission_{commission_id}_export.csv'"
 
         writer = csv.writer(http_response)
         # Write column titles for the CSV file
@@ -40,10 +50,53 @@ class CommissionProjectsCSVExport(generics.RetrieveAPIView):
         )
         # Add amount asked and earned for each fund
 
-        #        # Write CSV file content
-        #            writer.writerow(
-        #                [
-        #                ]
-        #            )
+        projects = queryset.filter(
+            id__in=ProjectCommissionFund.objects.filter(
+                commission_fund_id__in=CommissionFund.objects.filter(
+                    commission_id=commission_id
+                ).values("id")
+            )
+        ).order_by("id")
+        for project in projects:
+            association = (
+                None
+                if project.association_id is None
+                else Association.objects.get(id=project.association_id).name
+            )
+            user = (
+                None
+                if project.user_id is None
+                else User.objects.get(id=project.user_id).username
+            )
+            commission = Commission.objects.get(id=commission_id).name
+            categories = list(
+                Category.objects.filter(
+                    id__in=ProjectCategory.objects.filter(
+                        project_id=project.id
+                    ).values_list("category_id")
+                ).values_list("name")
+            )
+            project_commission_funds = ProjectCommissionFund.objects.filter(
+                project_id=project.id
+            )
+            is_first_edition = True
+            for edition in project_commission_funds:
+                if not edition.is_first_edition:
+                    is_first_edition = False
+                    break
+
+            # Write CSV file content
+            writer.writerow(
+                [
+                    project.id,
+                    association,
+                    user,
+                    commission,
+                    project.planned_start_date,
+                    project.planned_end_date,
+                    is_first_edition,
+                    categories,
+                ]
+            )
 
         return http_response
