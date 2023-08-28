acad8e7bff60771710f5f1fa02fdeaa4b709efaf
Carlo Spiga <cspiga@unistra.fr>
2023-08-28 14:26:25 +0200

    ✨ Added size to DocumentSerializer.

--- plana/apps/documents/serializers/document.py
+++ plana/apps/documents/serializers/document.py
@@ -1,4 +1,6 @@
 """Serializers describing fields used on documents."""
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from plana.apps.documents.models.document import Document
@@ -6,6 +8,16 @@ from plana.apps.documents.models.document import Document
 
 class DocumentSerializer(serializers.ModelSerializer):
     """Main serializer."""
+
+    size = serializers.SerializerMethodField()
+
+    @extend_schema_field(OpenApiTypes.INT)
+    def get_size(self, document):
+        """Return file size."""
+        if document.path_template:
+            return document.path_template.size
+        else:
+            return 0
 
     class Meta:
         model = Document
