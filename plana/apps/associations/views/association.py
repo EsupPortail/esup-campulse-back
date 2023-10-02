2923cd5adb7b0fc837e81ca98598c557fa0c5191
Carlo Spiga <cspiga@unistra.fr>
2023-10-02 13:20:09 +0200

    ⏪️ Patching a charter also edits is_public.

--- plana/apps/associations/views/association.py
+++ plana/apps/associations/views/association.py
@@ -721,9 +721,11 @@ class AssociationStatusUpdate(generics.UpdateAPIView):
         }
         if request.data["charter_status"] == "CHARTER_VALIDATED":
             association.is_site = True
+            association.is_public = True
             association.save()
         elif request.data["charter_status"] == "CHARTER_REJECTED":
             association.is_site = False
+            association.is_public = False
             association.save()
         if request.data["charter_status"] in mail_templates_codes_by_status:
             template = MailTemplate.objects.get(code=mail_templates_codes_by_status[request.data["charter_status"]])
