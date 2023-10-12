89ae31701b1241a28ebd2905b56bcead7dfedfef
Carlo Spiga <cspiga@unistra.fr>
2023-10-12 10:53:25 +0200

    ✏️ Typos in contents.

--- plana/settings/base.py
+++ plana/settings/base.py
@@ -86,13 +86,13 @@ ALLOWED_HOSTS = []
 #########################
 
 # Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# https://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
 TIME_ZONE = "Europe/Paris"
 
 # Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
+# https://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = "fr-FR"
 
 SITE_ID = 1
