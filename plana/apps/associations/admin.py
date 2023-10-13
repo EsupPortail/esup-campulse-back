15e9f6efea070246c5937b329bbca9a7f24bf0c3
Carlo Spiga <cspiga@unistra.fr>
2023-10-13 09:41:22 +0200

    🚧 Starting adding fields in django admin interface.

--- plana/apps/associations/admin.py
+++ plana/apps/associations/admin.py
@@ -3,4 +3,18 @@ from django.contrib import admin
 from .models import ActivityField, Association
 
 admin.site.register(ActivityField)
-admin.site.register(Association)
+
+
+@admin.register(Association)
+class AssociationAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "acronym",
+        "email",
+        "institution",
+        "is_enabled",
+        "is_public",
+        "is_site",
+        "charter_status",
+        "can_submit_projects",
+    ]
