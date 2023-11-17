c4d4fc4e950ad095499f79f3ae6ae7c10a9d4087
Carlo Spiga <cspiga@unistra.fr>
2023-11-17 15:01:24 +0100

    🗃️ Wrong field changed on Content.

--- plana/apps/contents/models/content.py
+++ plana/apps/contents/models/content.py
@@ -8,11 +8,11 @@ class Content(models.Model):
 
     code = models.CharField(_("Code"), max_length=128, blank=False, null=False, unique=True)
     label = models.CharField(_("Label"), max_length=128, blank=False, null=False, unique=True)
-    title = models.CharField(_("Title"), max_length=512, default="", null=True)
-    header = models.TextField(_("Header"), default="", null=True)
+    title = models.CharField(_("Title"), max_length=512, default="", blank=True, null=True)
+    header = models.TextField(_("Header"), default="", blank=True, null=True)
     body = models.TextField(_("Body"), blank=False, null=False)
-    footer = models.TextField(_("Footer"), default="", null=True)
-    aside = models.TextField(_("Sidebar"), default="", null=True)
+    footer = models.TextField(_("Footer"), default="", blank=True, null=True)
+    aside = models.TextField(_("Sidebar"), default="", blank=True, null=True)
 
     def __str__(self):
         return self.code
