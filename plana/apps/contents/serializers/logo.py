8ca8f4193e229ee8701c4edf118043e035d865ed
Carlo Spiga <cspiga@unistra.fr>
2024-01-18 11:37:13 +0100

    ⚡️ Improved logo S3 URL rendering.

--- plana/apps/contents/serializers/logo.py
+++ plana/apps/contents/serializers/logo.py
@@ -1,4 +1,5 @@
 """Serializers describing fields used on logos."""
+from django.conf import settings
 from rest_framework import serializers
 
 from plana.apps.contents.models.logo import Logo
@@ -6,6 +7,12 @@ from plana.apps.contents.models.logo import Logo
 
 class LogoSerializer(serializers.ModelSerializer):
     """Main serializer."""
+
+    path_logo = serializers.SerializerMethodField("cached_logo_url")
+
+    def cached_logo_url(self, logo):
+        """Return cached logo URL instead of calculated one which is slower."""
+        return f"{settings.AWS_S3_ENDPOINT_URL}/{settings.AWS_STORAGE_BUCKET_NAME}/{logo.path_logo.name}"
 
     class Meta:
         model = Logo
