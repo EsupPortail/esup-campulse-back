86b0862376ea7e621ca8dbbdd6290db24a000a02
lea.stroh <lea.stroh@unistra.fr>
2025-02-28 10:10:00 +0100

    🐛 Fixed cron_daily

--- plana/management/commands/cron_daily.py
+++ plana/management/commands/cron_daily.py
@@ -8,12 +8,13 @@ from django.utils.translation import gettext as _
 class Command(BaseCommand):
     help = _("Global CRON daily launched.")
 
-    call_command("cron_account_expiration")
-    call_command("cron_association_expiration")
-    call_command("cron_commission_expiration")
-    call_command("cron_document_expiration")
-    call_command("cron_goa_expiration")
-    call_command("cron_history_expiration")
-    call_command("cron_password_expiration")
-    call_command("cron_project_expiration")
-    call_command("cron_review_expiration")
+    def handle(self, *args, **options):
+        call_command("cron_account_expiration")
+        call_command("cron_association_expiration")
+        call_command("cron_commission_expiration")
+        call_command("cron_document_expiration")
+        call_command("cron_goa_expiration")
+        call_command("cron_history_expiration")
+        call_command("cron_password_expiration")
+        call_command("cron_project_expiration")
+        call_command("cron_review_expiration")
