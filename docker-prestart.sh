47325a194d7ee840a1b3b7704d0b61b50567834b
lea.stroh <lea.stroh@unistra.fr>
2025-09-03 15:39:26 +0200

    🔧 Removed generate age and jwt keys from docker-prestart

--- docker-prestart.sh
+++ docker-prestart.sh
@@ -7,16 +7,6 @@ cd /app
 python manage.py migrate
 python manage.py compilemessages || true
 
-if [ ! -f keys/jwt-private-key.pem ]
-then
-    python manage.py generate_jwt_keys
-fi
-
-if [ ! -f keys/age-private-key.key ]
-then
-    python manage.py generate_age_keys
-fi
-
 python manage.py initial_import --storages
 # env
 
