29dbc8e3115d242054bb3da63984d0e5d6e5c623
lea.stroh <lea.stroh@unistra.fr>
2025-11-21 16:51:07 +0100

    🐛 Fixed s3static templatetag to match S3 file paths

--- plana/templatetags/plana_tags.py
+++ plana/templatetags/plana_tags.py
@@ -8,10 +8,14 @@ from plana.utils import get_s3_client
 register = template.Library()
 
 
-# FIXME : PDF templates in public or private bucket ?
-@register.simple_tag
 def s3static(object_key):
     """Call with {% s3static 'file/path' %}."""
+    object_key = f"pdf/{object_key}"
     return get_s3_client().generate_presigned_url(
-        'get_object', Params={'Bucket': settings.AWS_STORAGE_PUBLIC_BUCKET_NAME, 'Key': object_key}, ExpiresIn=3600
+        "get_object",
+        Params={
+            "Bucket": settings.AWS_STORAGE_PUBLIC_BUCKET_NAME,
+            "Key": object_key,
+        },
+        ExpiresIn=3600,
     )
