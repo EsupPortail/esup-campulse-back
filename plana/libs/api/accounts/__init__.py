8ed0053970b50f867ff42b8bed370bf692bf7df7
lea.stroh <lea.stroh@unistra.fr>
2026-09-15 15:37:39 +0200

    🐛 Fixed client_path for external ldap accounts lib

--- plana/libs/api/accounts/__init__.py
+++ plana/libs/api/accounts/__init__.py
@@ -14,7 +14,7 @@ from django.conf import settings
 
 
 def get_client():
-    client_path = f"plana.libs.api.accounts.{settings.ACCOUNTS_API_CLIENT}AccountsClient"
+    client_path = f"plana.libs.api.accounts.{settings.ACCOUNTS_API_CLIENT.lower()}.{settings.ACCOUNTS_API_CLIENT}AccountsAPI"
     mod, package = client_path.rsplit('.', 1)
     client_class = getattr(import_module(mod), package)
     return client_class
